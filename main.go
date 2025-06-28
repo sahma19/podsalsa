9be642588e41b1ffcb218372b6d0d3ec8794c759
sahma19 <55732914+sahma19@users.noreply.github.com>
2025-06-29 00:38:24 +0200

    Update main.go

--- main.go
+++ main.go
@@ -97,7 +97,11 @@ func main() {
 	srv, _ := http.NewServer(&srvCfg, logger)
 	httpServer := srv.ListenAndServe(channel)
 	if httpServer != nil {
-		defer httpServer.Close()
+		defer func() {
+			if err := httpServer.Close(); err != nil {
+				logger.Warn("failed to close http server", "err", err)
+			}
+		}()
 	}
 
 	// wait for shutdown signal
